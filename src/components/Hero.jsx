87251cb43c598dcca07bcbe21db4519f70057c9f
Syazwan Fitri <syazfitry69@gmail.com>
2022-10-25 15:35:23 +0800

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import './Hero.css';
+
+const Hero = () => {
+    return (
+        <div className="hero-container">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Hero;
