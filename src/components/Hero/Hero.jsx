22a5ea099168b0368ae60ae7bcdff883231caa69
Syazwan Fitri <syazfitry69@gmail.com>
2022-10-27 14:27:17 +0800

    Added .hero-desc, style .hero-desc elements

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,22 @@
 import Navbar from "../Navbar/Navbar";
 import './Hero.css';
+import Men from '../../images/hero_illustr.svg'
 
 const Hero = () => {
     return (
         <div className="hero-container">
             <Navbar />
+            <div className="hero-desc">
+                <div className="hero-desc-left">
+                    <h1>Your next big
+                        idea starts here</h1>
+                    <h3>The ideal framework to learn how to manage all aspects of startup.</h3>
+                    <button className="btnStart">Start For Free</button>
+                </div>
+                <div className="hero-desc-right">
+                    <img src={Men} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
