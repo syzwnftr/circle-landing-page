15dbd0cfa3a8e39380e5d6d490385a71660b0f44
Syazwan Fitri <syazfitry69@gmail.com>
2022-10-28 15:01:31 +0800

    Added classname, added animation to the leaves

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../Navbar/Navbar";
 import './Hero.css';
 import Men from '../../images/hero_illustr.svg';
-import Logo1 from '../../images/hero_logo-01.svg'
-import Logo2 from '../../images/hero_logo-02.svg'
-import Logo3 from '../../images/hero_logo-03.svg'
-import Logo4 from '../../images/hero_logo-04.svg'
-import Plane from '../../images/hero_illustr-plane.svg'
+import Logo1 from '../../images/hero_logo-01.svg';
+import Logo2 from '../../images/hero_logo-02.svg';
+import Logo3 from '../../images/hero_logo-03.svg';
+import Logo4 from '../../images/hero_logo-04.svg';
+import Plane from '../../images/hero_illustr-plane.svg';
+import Leave1 from '../../images/leaves-1.svg';
+import Leave2 from '../../images/leaves-2.svg';
 
 const Hero = () => {
     return (
@@ -19,8 +21,10 @@ const Hero = () => {
                     <button className="btnStart">Start For Free</button>
                 </div>
                 <div className="hero-desc-right">
-                    <img src={Men} alt="" />
+                    <img src={Men} alt="" className="men" />
                     <img src={Plane} alt="" className="hero-plane" />
+                    <img src={Leave1} alt="" className="leave-1" />
+                    <img src={Leave2} alt="" className="leave-2" />
                 </div>
             </div>
             <div className="hero-logo-wrapper">
