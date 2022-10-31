0e05170d0394cd91d9dc4d4328562a77928c231b
Syazwan Fitri <syazfitry69@gmail.com>
2022-10-31 23:15:29 +0800

    Added Syllabus component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Syllabus from "./components/Syllabus/Syllabus";
 
 const App = () => {
   return (
     <div className="app">
       <Hero />
+      <Syllabus />
     </div>
   );
 };
