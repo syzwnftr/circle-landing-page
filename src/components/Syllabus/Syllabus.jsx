63b2c7b18122ce914c612acd90bb51fdbebca929
Syazwan Fitri <syazfitry69@gmail.com>
2022-11-01 12:23:30 +0800

    Added syllabus content

--- src/components/Syllabus/Syllabus.jsx
+++ src/components/Syllabus/Syllabus.jsx
@@ -3,6 +3,9 @@ import Lady from '../../images/computer-couch.png';
 import Num1 from '../../images/step_numb-01.svg';
 import Num2 from '../../images/step_numb-02.svg';
 import Num3 from '../../images/step_numb-03.svg';
+import Arrow from '../../images/steps_arrow.svg';
+import ArrowPlane from '../../images/steps_arrow-plane.svg';
+
 
 const Syllabus = () => {
     return (
@@ -13,6 +16,47 @@ const Syllabus = () => {
 
             <div className="syllabus-step">
                 <h2>Syllabus of courses</h2>
+                <div className="syllabus-items-container">
+                    <div className="syllabus-items">
+                        <div className="syllabus-num">
+                            <div className="num-text">01</div>
+                            <img src={Num1} alt="" />
+                        </div>
+                        <div className="items-desc">
+                            <h3 className="item-title">Videos from professionals</h3>
+                            <p className="item-text">Our specialists will help you create any website. With our help, you can handle any task, also you get a personalized consultation.</p>
+                        </div>
+                        <div className="syllabus-arrow">
+                            <img src={Arrow} alt="" />
+                        </div>
+                    </div>
+                    <div className="syllabus-items">
+                        <div className="syllabus-arrow arrow-two">
+                            <img src={Arrow} alt="" />
+                        </div>
+                        <div className="syllabus-num">
+                            <div className="num-text">02</div>
+                            <img src={Num2} alt="" />
+                        </div>
+                        <div className="items-desc">
+                            <h3 className="item-title">Practical independent work</h3>
+                            <p className="item-text">Learn how to organize your website, create engaging content, protect your site, and achieve search engine rankings.</p>
+                        </div>
+                    </div>
+                    <div className="syllabus-items">
+                        <div className="syllabus-num">
+                            <div className="num-text">03</div>
+                            <img src={Num3} alt="" />
+                        </div>
+                        <div className="items-desc">
+                            <h3 className="item-title">Feedback from specialists</h3>
+                            <p className="item-text">Our Structure and visualize new knowledge. You send the practical work to the reviewing specialists and receive individual feedback.</p>
+                        </div>
+                        <div className="syllabus-arrow">
+                            <img src={ArrowPlane} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
