131c6d7b946dd8bacfe0fee7f587d9d52bb21695
Syazwan Fitri <syazfitry69@gmail.com>
2022-11-03 13:41:39 +0800

    Added .clock-hand, few classes

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,5 +1,11 @@
 import './Courses.css';
 import ArrowBlue from '../../images/ico_arrow-blue.svg';
+import Clock from '../../images/courses_illustr-clock.svg';
+import Graph1 from '../../images/courses_illustr-graph.svg';
+import Graph2 from '../../images/courses_illustr-graph2.svg';
+import Screen1 from '../../images/courses_illustr-screen.svg';
+import Screen2 from '../../images/courses_illustr-screen2.svg';
+
 
 const Courses = () => {
     return (
@@ -23,6 +29,10 @@ const Courses = () => {
                         <a href="#">Read More</a>
                         <img src={ArrowBlue} alt="" className='arrowBlue' />
                     </div>
+                    <div className="courses-item-illus">
+                        <div className="clock-hand"></div>
+                        <img src={Clock} alt="" />
+                    </div>
                 </div>
                 <div className="courses-item programming">
                     <div className="courses-item-desc">
@@ -33,6 +43,10 @@ const Courses = () => {
                         <a href="#">Read More</a>
                         <img src={ArrowBlue} alt="" className='arrowBlue' />
                     </div>
+                    <div className="courses-item-illus">
+                        <img src={Screen2} alt="" className='illus-one' />
+                        <img src={Screen1} alt="" className='illus-two' />
+                    </div>
                 </div>
                 <div className="courses-item seo">
                     <div className="courses-item-desc">
@@ -42,6 +56,10 @@ const Courses = () => {
                     <div className="read-more">
                         <a href="#">Read More</a>
                         <img src={ArrowBlue} alt="" className='arrowBlue' />
+                    </div>
+                    <div className="courses-item-illus">
+                        <img src={Graph1} alt="" className='illus-one' />
+                        <img src={Graph2} alt="" className='illus-two' />
                     </div>
                 </div>
             </div>
