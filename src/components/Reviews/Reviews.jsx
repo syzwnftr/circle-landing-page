b0905a5aee2af9e7592798acf9c774676d0f853d
Syazwan Fitri <syazfitry69@gmail.com>
2022-11-04 14:45:29 +0800

    Added img, classname

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,6 +2,7 @@ import './Reviews.css';
 import Anna from '../../images/ava-03.png';
 import Mykola from '../../images/ava-02.png';
 import Natsya from '../../images/ava-01.png';
+import Spot from '../../images/spot.svg';
 
 const Reviews = () => {
     return (
@@ -14,28 +15,44 @@ const Reviews = () => {
                     <div className="reviews-item">
                         <div className="reviews-ava">
                             <img src={Anna} alt="" />
+                            <img src={Spot} alt="" className='spot anna-spot' />
+                            <div className="black-outline bo-anna"></div>
+                            <div className="black-dot bd-anna"></div>
                         </div>
                         <div className="reviews-desc">
-                            <h3 className="student-name">Anna Tyuneva</h3>
-                            <p className='student-review'>The course is great! Teachers talks very interesting and accessible. Thank you very much!</p>
+                            <h4 className="student-name">Anna Tyuneva</h4>
+                            <div className="sr-div">
+                                <p className='student-review'>The course is great! Teachers talks very interesting and accessible. Thank you very much!</p>
+
+                            </div>
                         </div>
                     </div>
                     <div className="reviews-item">
                         <div className="reviews-ava">
                             <img src={Mykola} alt="" />
+                            <img src={Spot} alt="" className='spot mykola-spot' />
+                            <div className="black-outline bo-mykola"></div>
+                            <div className="black-dot bd-mykola"></div>
                         </div>
                         <div className="reviews-desc">
-                            <h3 className="student-name">Mykola Dunayev</h3>
-                            <p className='student-review'>The course is clear enough. Well explained a lot of practice. I recommend to everyone!</p>
+                            <h4 className="student-name">Mykola Dunayev</h4>
+                            <div className="sr-div">
+                                <p className='student-review'>The course is clear enough. Well explained a lot of practice. I recommend to everyone!</p>
+                            </div>
                         </div>
                     </div>
                     <div className="reviews-item">
                         <div className="reviews-ava">
                             <img src={Natsya} alt="" />
+                            <img src={Spot} alt="" className='spot natsya-spot' />
+                            <div className="black-outline bo-natsya"></div>
+                            <div className="black-dot bd-natsya"></div>
                         </div>
                         <div className="reviews-desc">
-                            <h3 className="student-name">Nastya Kozarchuk</h3>
-                            <p className='student-review'>he training was in one breath. Very accessible courses, everything is very clear and good.</p>
+                            <h4 className="student-name">Nastya Kozarchuk</h4>
+                            <div className="sr-div">
+                                <p className='student-review'>The training was in one breath. Very accessible courses, everything is very clear and good.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
