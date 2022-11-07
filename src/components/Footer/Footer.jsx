2d7dc764b3887b45c6a84d11a9270fd14d17eb61
Syazwan Fitri <syazfitry69@gmail.com>
2022-11-07 15:37:01 +0800

    Added content and style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,21 +13,27 @@ const Footer = () => {
                 <div className="footer-nav">
                     <div className="footer-nav-items">
                         <a href='#'>About</a>
-                        <a href='#'>Program</a>
+                        <a href='#' className='a-last'>Program</a>
                     </div>
                     <div className="footer-nav-items">
                         <a href='#'>Courses</a>
-                        <a href='#'>Reviews</a>
+                        <a href='#' className='a-last'>Reviews</a>
+                    </div>
+                    <div className="dash-vertical">
+                        <img src={DashVertical} alt="" />
+                    </div>
+                    <div className="dash-vertical two">
+                        <img src={DashVertical} alt="" />
                     </div>
                 </div>
                 <div className="footer-contact">
                     <div className="footer-contact-items">
                         <a href='#'>www.halo-lab.com</a>
-                        <a href='#'>mail@halo-lab.com</a>
+                        <a href='#' className='a-last'>mail@halo-lab.com</a>
                     </div>
                     <div className="footer-contact-items">
                         <a href='#'>+38 096 272 2100</a>
-                        <p>Ukraine, Odessa</p>
+                        <p className='a-last'>Ukraine, Odessa</p>
                     </div>
                 </div>
             </div>
